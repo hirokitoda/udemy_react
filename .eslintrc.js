26587523c66b35376d146bcfa1cf5211aeb77556
hirokitoda <hirokimusic123@gmail.com>
2018-06-21 18:08:04 +0900

    rec16 fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,6 @@
 module.exports = {
-<<<<<<< HEAD
-    "extends": "airbnb",
-    "env": {
-=======
   "extends": "airbnb",
   "env": {
->>>>>>> 30918020e1f6b4b9257d8423d91f245bcef22d2e
     "browser": true
   }
 };
